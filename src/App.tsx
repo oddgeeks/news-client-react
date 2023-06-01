dc523620bbec9f1249c3b97df059c44cc38bc76e
oddgeeks <oddgeeks.dev@gmail.com>
2023-06-01 17:36:38 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
